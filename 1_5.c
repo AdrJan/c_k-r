7ccbc3dd4d59cd992b7ee0e81ffa43a1e7254a79
Adrian Jankowski <adrianjankowski@MacBook-Pro-Adrian.local>
2020-05-29 12:15:09 +0200

    Added 1-5 exercise.

---
+++ 1_5.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+void printHeader();
+void printConv();
+
+int main() {
+    printHeader();
+    printConv();
+}
+
+void printHeader() {
+    printf("----CONVERSION----\n");
+    printf("Fahrenheit Celsius\n");
+}
+
+void printConv() {
+    float fahr, celsius;
+    int lower, upper, step;
+
+    lower = 0;
+    upper = 300;
+    step = 20;
+
+    fahr = upper;
+    while(fahr >= lower) {
+        celsius = (5.0/9.0) * (fahr-32.0);
+        printf("%10.0f %7.1f\n", fahr, celsius);
+        fahr = fahr - step;
+    }
+}
