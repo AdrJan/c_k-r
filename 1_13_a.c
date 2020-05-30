e04869ac5817ea35f0238e9e18283eb0035fd93e
Adrian Jankowski <adrianjankowski@MacBook-Pro-Adrian.local>
2020-05-30 19:22:20 +0200

    Refactor for 1-13 first version.

--- 1_13_a.c
+++ 1_13_a.c
@@ -6,35 +6,41 @@
 #define MAX_WORD_LENGTH 100
 #define BAR_RESOLUTION 100
 
-void fill_array_zeros(int[], int);
+void fill_array_zeros(int[]);
 void get_data(int[]);
 void print_header();
-void print_horizontal_histogram(int[], int);
-int get_total_word_length(int[], int);
-void print_histogram_line(int, int, int);
+void print_horizontal_histogram(int[]);
+int get_total_word_length(int[]);
+void print_histogram_line(int, int);
 
-int main() {
+int main() 
+{
     int word_lengths[MAX_WORD_LENGTH];
 
-    fill_array_zeros(word_lengths, MAX_WORD_LENGTH);
+    fill_array_zeros(word_lengths);
     get_data(word_lengths);
     print_header();
-    print_horizontal_histogram(word_lengths, MAX_WORD_LENGTH);
+    print_horizontal_histogram(word_lengths);
 }
 
-void fill_array_zeros(int arr[], int max) {
-    for(int i = 0; i < max; i++) 
+void fill_array_zeros(int arr[]) 
+{
+    for(int i = 0; i < MAX_WORD_LENGTH; i++) 
         arr[i] = 0;
 } 
 
-void get_data(int word_lengths[]) {
+void get_data(int word_lengths[]) 
+{
     int c;
     int ws_state = IS_WHITESPACE;
     int word_length = 0;
 
-    while((c = getchar()) != EOF) {
-        if(c == ' ' || c == '\n' || c == '\t') {
-            if(!ws_state) {
+    while((c = getchar()) != EOF) 
+    {
+        if(c == ' ' || c == '\n' || c == '\t') 
+        {
+            if(!ws_state) 
+            {
                 word_lengths[word_length] += 1;
                 word_length = 0;
             }
@@ -50,29 +56,33 @@ void print_header() {
     printf("=== WORD LENGTH HISTOGRAM ===\n");
 }
 
-void print_horizontal_histogram(int words_length[], int max) {
-    int total = get_total_word_length(words_length, max);
-
-    for(int i = 0; i < max; i++) {
-        if(words_length[i] > 0) {
+void print_horizontal_histogram(int words_length[]) 
+{
+    int total = get_total_word_length(words_length);
+    for(int i = 0; i < MAX_WORD_LENGTH; i++) 
+    {
+        if(words_length[i] > 0) 
+        {
             printf("[%3d]", i);
-            print_histogram_line(words_length[i], total, BAR_RESOLUTION);
+            print_histogram_line(words_length[i], total);
         }
     }
 }
 
-void print_histogram_line(int word_len_cnt, int total, int res) {
-    int bar_len = round(((float) word_len_cnt / total) * res);
-
+void print_histogram_line(int word_len_cnt, int total) 
+{
+    int bar_len = round(((float) word_len_cnt / total) * BAR_RESOLUTION);
     printf("|");
     for(int i = 0; i < bar_len; i++)
         printf("=");
     printf("\n");
 }
 
-int get_total_word_length(int word_lengths[], int max) {
+int get_total_word_length(int word_lengths[])
+ {
     int total = 0;
-    for(int i = 0; i < max; i++) {
+    for(int i = 0; i < MAX_WORD_LENGTH; i++) 
+    {
         total += word_lengths[i];
     }
     return total;
