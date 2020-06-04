b46786f899046dbec190a3bb8ecbce4198527588
Adrian Jankowski <adrianjankowski@MacBook-Pro-Adrian.local>
2020-06-05 00:40:33 +0200

    Fix for exercise 2-6.

--- 2_6.c
+++ 2_6.c
@@ -6,14 +6,14 @@ unsigned setbits(unsigned, int, int, unsigned);
 
 int main()
 {
-    assert(setbits(0b1, 4, 1, 0b101) == 17);
-    assert(setbits(0b101, 4, 0, 0b1111) ==  5);
-    assert(setbits(0b100001, 2, 3, 0b101) == 53);
-    
+    assert(setbits(0b1, 4, 1, 0b101) == 0b10001);
+    assert(setbits(0b101, 4, 0, 0b1111) == 0b101);
+    assert(setbits(0b100001, 2, 3, 0b101) == 0b100101);
+
     return EXIT_SUCCESS;
 }
 
 unsigned setbits(unsigned x, int p, int n, unsigned y)
 {
-    return  ((~(~0<<n) & y) << p) | ((~(~(~0<<n) << p)) & x);
+    return ((~(~0 << n) & y) << (p + 1 - n)) | ((~(~(~0 << n) << (p + 1 - n))) & x);
 }
