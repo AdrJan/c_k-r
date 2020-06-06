17ce0567fe3cd1e47a2e8c79df2f5f31aea858e4
Adrian Jankowski <jankowski.adrian13@gmail.com>
2020-06-07 00:17:09 +0200

    Added exercise 3-2. Solution.

--- 3_2.c
+++ 3_2.c
@@ -4,15 +4,20 @@
 #define MAX_LEN 100000
 
 void escape(char[], char[]);
+void escape_rev(char[], char[]);
 
 int main()
 {
-    char s[MAX_LEN] = "It is example with tab \t and few newlines \n\n end \n\t.";
+    char s[MAX_LEN] = "\t\t\t    \\TEST\\    \n\n\n";
     char t[MAX_LEN];
 
     escape(s, t);
     printf("Text to escape: %s\n", s);
     printf("Text escaped: %s\n", t);
+
+    escape_rev(t, s);
+    printf("Text to reverse-escape: %s\n", t);
+    printf("Text reverse-escaped: %s\n", s);
 
     return EXIT_SUCCESS;
 }
@@ -34,6 +39,40 @@ void escape(char s[], char t[])
         case '\t':
             t[j++] = '\\';
             t[j] = 't';
+            break;
+        default:
+            t[j] = s[i];
+            break;
+        }
+        i++;
+        j++;
+    }
+}
+
+void escape_rev(char s[], char t[])
+{
+    int i, j;
+    i = j = 0;
+
+    while (s[i] != '\0')
+    {
+        switch (s[i])
+        {
+        case '\\':
+            switch (s[i + 1])
+            {
+            case 'n':
+                t[j] = '\n';
+                break;
+            case 't':
+                t[j] = '\t';
+                break;
+            default:
+                t[j++] = '\\';
+                t[j] = s[i + 1];
+                break;
+            }
+            i++;
             break;
         default:
             t[j] = s[i];
